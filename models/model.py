f931c317385a63eb92dd36eca3e8e9c56ba6875d
heyoni <hhheyoni@gmail.com>
2023-04-30 23:59:42 +0900

    1-3, 1-4. 값 객체외 엔티티, 도메인 서비스 함수
    - 값 객체와 엔티티에 대한 개념
    - 도메인 서비스 함수를 통해 모든 것을 객체로 만들 필요는 없다는 것을 알려줌

--- models/model.py
+++ models/model.py
@@ -1,25 +1,62 @@
-from database import Base
-from sqlalchemy import Column, Integer, String
-from typing import Optional
+from __future__ import annotations
+from dataclasses import dataclass
 from datetime import date
+from typing import Optional, List, Set
 
 
-class OrderLine(Base):
-    order_id = Column(String(20), nullable=False)
-    sku = Column(String(50), nullable=False)
-    qty = Column(Integer, nullable=False)
+class OutOfStock(Exception):
+    pass
+
+
+def allocate(line: OrderLine, batches: List[Batch]) -> str:
+    try:
+        batch = next(b for b in sorted(batches) if b.can_allocate(line))
+        batch.allocate(line)
+        return batch.reference
+    except StopIteration:
+        raise OutOfStock(f"Out of stock for sku {line.sku}")
+
+
+@dataclass(frozen=True)
+class OrderLine:
+    orderid: str
+    sku: str
+    qty: int
 
 
 class Batch:
     def __init__(self, ref: str, sku: str, qty: int, eta: Optional[date]):
-        self.refrence = ref
+        self.reference = ref
         self.sku = sku
         self.eta = eta
         self._purchased_quantity = qty
-        self._allocations = set()
+        self._allocations = set()  # type: Set[OrderLine]
 
-    def allocate(self, line: OrderLine) -> None:
-        self.available_quantity -= line.qty
+    def __repr__(self):
+        return f"<Batch {self.reference}>"
+
+    def __eq__(self, other):
+        if not isinstance(other, Batch):
+            return False
+        return other.reference == self.reference
+
+    def __hash__(self):
+        return hash(self.reference)
+
+    def __gt__(self, other):
+        if self.eta is None:
+            return False
+        if other.eta is None:
+            return True
+        return self.eta > other.eta
+
+    def allocate(self, line: OrderLine):
+        if self.can_allocate(line):
+            self._allocations.add(line)
+
+    def deallocate(self, line: OrderLine):
+        if line in self._allocations:
+            self._allocations.remove(line)
 
     @property
     def allocated_quantity(self) -> int:
@@ -29,6 +66,5 @@ class Batch:
     def available_quantity(self) -> int:
         return self._purchased_quantity - self.allocated_quantity
 
-    def can_allocate(self, line) -> bool:
+    def can_allocate(self, line: OrderLine) -> bool:
         return self.sku == line.sku and self.available_quantity >= line.qty
-    
